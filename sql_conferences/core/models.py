556561d6b331034e7c9e122ef0f12dea657f960b
Misha Korotia <mr.anderson528491@gmail.com>
2025-09-11 11:20:37 +0300

    Added models according to the logic

---
+++ sql_conferences/core/models.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from datetime import date, datetime
+
+
+@dataclass
+class Conference:
+    id: int
+    name: str
+    start_date: date
+    end_date: date
+    building: str
+
+
+@dataclass
+class Section:
+    id: int
+    name: str
+    number: int
+    moderator: str
+    hall: str
+    conference_id: int
+
+
+@dataclass
+class Speaker:
+    id: int
+    full_name: str
+    bio: str
+    academic_degree: str
+    workplace: str
+    position: str
+
+
+@dataclass
+class Presentation:
+    id: int
+    topic: str
+    start_time: datetime
+    duration_minutes: int
+    section_id: int
+    speaker_id: int
+
+
+@dataclass
+class Equipment:
+    id: int
+    name: str
+
+
+@dataclass
+class PresentationEquipment:
+    quantity: int
+    presentation_id: int
+    equipment_id: int
