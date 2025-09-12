01a31811ce745bb9b478bfe04ee4c1766def9845
Misha Korotia <mr.anderson528491@gmail.com>
2025-09-12 20:13:27 +0300

    Updated logic in add_conference method

--- sql_conferences/crud/insert_data.py
+++ sql_conferences/crud/insert_data.py
@@ -10,18 +10,19 @@ class InsertData:
     def add_conference(self, name, start_date, end_date, building):
         query = """
                 INSERT INTO Conference(name, start_date, end_date, building)
-                VALUES (%s, %s, %s, %s) 
-                RETURNING id;
+                VALUES (%s, %s, %s, %s) RETURNING id;
                 """
-
-        self.cursor.execute(query, (name, start_date, end_date, building))
-        return self.cursor.fetchone()[0]
+        try:
+            self.cursor.execute(query, (name, start_date, end_date, building))
+            return self.cursor.fetchone()[0]
+        except Exception as e:
+            print(f"Error occured: {e}")
+            raise
 
     def add_section(self, name, number, moderator, hall, conference_id):
         query = """
                 INSERT INTO Section (name, number, moderator, hall, conference_id)
-                VALUES (%s, %s, %s, %s, %s) 
-                RETURNING id;
+                VALUES (%s, %s, %s, %s, %s) RETURNING id;
                 """
         self.cursor.execute(query, (name, number, moderator, hall, conference_id))
         return self.cursor.fetchone()[0]
@@ -29,8 +30,7 @@ class InsertData:
     def add_speaker(self, full_name, bio, academic_degree, workplace, position):
         query = """
                 INSERT INTO Speaker(full_name, bio, academic_degree, workplace, position)
-                VALUES (%s, %s, %s, %s, %s) 
-                RETURNING id;
+                VALUES (%s, %s, %s, %s, %s) RETURNING id;
                 """
         self.cursor.execute(query, (full_name, bio, academic_degree, workplace, position))
         return self.cursor.fetchone()[0]
@@ -40,8 +40,7 @@ class InsertData:
         hall_availability(self.cursor, section_id, start_time, duration_minutes)
         query = """
                 INSERT INTO Presentation(topic, start_time, duration_minutes, section_id, speaker_id)
-                VALUES (%s, %s, %s, %s, %s) 
-                RETURNING id;
+                VALUES (%s, %s, %s, %s, %s) RETURNING id;
                 """
         self.cursor.execute(query, (topic, start_time, duration_minutes, section_id, speaker_id))
         return self.cursor.fetchone()[0]
@@ -49,8 +48,7 @@ class InsertData:
     def add_equipment(self, name):
         query = """
                 INSERT INTO Equipment(name)
-                VALUES (%s) 
-                RETURNING id;
+                VALUES (%s) RETURNING id;
                 """
         self.cursor.execute(query, (name,))
         return self.cursor.fetchone()[0]
